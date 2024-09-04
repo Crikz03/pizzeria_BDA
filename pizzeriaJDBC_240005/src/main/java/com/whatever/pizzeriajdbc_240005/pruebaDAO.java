0552430ca871d6fd4bee393b3677d4e417e1e16a
CriCrin03 <chris012203@gmail.com>
2024-09-04 01:58:44 -0700

    prueba de dao completado

--- pizzeriaJDBC_240005/src/main/java/com/whatever/pizzeriajdbc_240005/pruebaDAO.java
+++ pizzeriaJDBC_240005/src/main/java/com/whatever/pizzeriajdbc_240005/pruebaDAO.java
@@ -4,6 +4,12 @@
  */
 package com.whatever.pizzeriajdbc_240005;
 
+import Conexion.ConexionBD;
+import Conexion.IConexionBD;
+import DAO.ProductoDAO;
+import Interfaces.IProductoDAO;
+import Objetos.Producto;
+
 /**
  *
  * @author Chris
@@ -14,7 +20,24 @@ public class pruebaDAO {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        IConexionBD conexionBD = new ConexionBD();
+        IProductoDAO productoDAO = new ProductoDAO(conexionBD);
+
+        Producto p1 = new Producto("Pizza de champiñones", "Pizza senccilla con base de tomate y queso, con topping de champiñones.", 120.00f);
+
+        Producto p2 = new Producto("Pizza de extra queso", "Pizza senccilla con base de tomate y queso, con topping de extra queso.", 100.00f);
+
+        Producto p3 = new Producto("Pizza clasica", "Pizza senccilla con base de tomate y queso", 90.00f);
+
+//        productoDAO.agregar(p1);
+//        productoDAO.agregar(p2);
+//        productoDAO.agregar(p3);
+
+//        productoDAO.eliminar(2);
+
+//        System.out.println(productoDAO.consultar(3));
+
+        System.out.println(productoDAO.cconsultarTodo());
     }
-    
+
 }
